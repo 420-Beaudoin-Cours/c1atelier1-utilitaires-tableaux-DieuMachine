7af043713b553677818e880a39bbc19cf4914124
DieuMachine <lindenb477@hotmail.com>
2020-01-24 05:29:18 -0500

    test1

--- Tableaux.java
+++ Tableaux.java
@@ -39,4 +39,53 @@ public class Tableaux {
                 permute(tab, i, imin);
         }
     }
+
+    public static void triDecroissantSSS(int[] tab){
+        int imin;
+        for (int i=0; i<tab.length-1; i++){
+            imin = i;
+            for (int j=i+1; j<tab.length; j++){
+                if (tab[j] > tab[imin])
+                    imin = j;
+            }
+            if (imin != i)
+                permute(tab, i, imin);
+        }
+    }
+
+    public static void print(int[] tab){
+        for (int elem : tab)
+            System.out.println(elem);
+    }
+
+    public static int maximum(int[] tab){
+        int index = 0;
+        for(int i = 1; i < tab.length; i++){
+            if (tab[i] > tab[index])
+                index = i;
+        }
+        return index;
+    }
+    public static int minimum(int[] tab){
+        int index = 0;
+        for(int i = 1; i < tab.length; i++){
+            if (tab[i] < tab[index])
+                index = i;
+        }
+        return index;
+    }
+
+    public static int moyenne(int[] tab){
+        int i = 0;
+        for (int elem : tab)
+            i+=elem;
+        try {
+            i/=tab.length;
+        }
+        catch (ArithmeticException e){
+
+        }
+        return i;
+    }
+
 }
